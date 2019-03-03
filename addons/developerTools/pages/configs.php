29cfe60ba1b8c0c672baf68ee53eba8932272c1e
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-03-03 18:47:59 +0500

    Added UI for server configs page

--- addons/developerTools/pages/configs.php
+++ addons/developerTools/pages/configs.php
@@ -1,7 +1,16 @@
 <?php
 
-  /**
-  *
-  */
+$addons = new Addons();
 
-?>
+$response = array();
+$settings = array('upload_max_filesize', 'max_execution_time', 'post_max_size', 'memory_limit');
+foreach ($settings as $key => $value) {
+  $response[$value] = ini_get($value);
+}
+
+$response['core_url'] = CORE_URL;
+$response['core_directory'] = CORE_DIRECTORY;
+$parameters['response'] = $response;
+$parameters['_title'] = 'Server Configs';
+$parameters['mainSection'] = 'developer_tools';
+$addons->display(DEV_CORE_NAME, 'pages/configs.html', $parameters);
