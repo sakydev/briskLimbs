883c948d016ba8410bd18783d0b9337461a1a3ea
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-03-10 00:13:10 +0500

    Ads table added Comments table added Videos columns modified

--- installer/imports/ads.sql
+++ installer/imports/ads.sql
@@ -16,37 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `videos`
+-- Table structure for table `ads`
 --
 
-DROP TABLE IF EXISTS `videos`;
+DROP TABLE IF EXISTS `ads`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `videos` (
-  `id` int(11) NOT NULL,
-  `vkey` varchar(10) NOT NULL,
-  `filename` varchar(15) NOT NULL,
-  `uploader_id` int(11) NOT NULL,
-  `uploader_name` varchar(100) NOT NULL,
-  `date` datetime DEFAULT CURRENT_TIMESTAMP,
-  `title` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  `status` enum('pending','failed','successful') DEFAULT 'pending',
-  `qualities` varchar(20) NOT NULL,
-  `duration` int(11) NOT NULL,
-  `thumbnails_count` int(11) NOT NULL,
-  `views` int(11) NOT NULL,
-  `comments` int(5) NOT NULL,
-  `state` enum('active','inactive','disabled') DEFAULT 'active',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `video_key` (`vkey`),
-  FULLTEXT KEY `title` (`title`,`description`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `ads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `status` enum('active','inactive') DEFAULT 'active',
+  `location` enum('home_banner','search_sidebar','watch_sidebar','watch_banner','custom') NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `videos`
+-- Dumping data for table `ads`
 --
+
+LOCK TABLES `ads` WRITE;
+/*!40000 ALTER TABLE `ads` DISABLE KEYS */;
+INSERT INTO `ads` VALUES (2,'search_sidebar','active','search_sidebar','default'),(4,'home_sidebar','active','home_banner','default'),(5,'watch_sidebar','active','watch_sidebar','default'),(7,'watch_banner','active','watch_banner','default');
+/*!40000 ALTER TABLE `ads` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -56,4 +51,4 @@ CREATE TABLE `videos` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-10  0:11:06
+-- Dump completed on 2019-03-10  0:10:33
--- installer/imports/comments.sql
+++ installer/imports/comments.sql
@@ -16,37 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `videos`
+-- Table structure for table `comments`
 --
 
-DROP TABLE IF EXISTS `videos`;
+DROP TABLE IF EXISTS `comments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `videos` (
-  `id` int(11) NOT NULL,
-  `vkey` varchar(10) NOT NULL,
-  `filename` varchar(15) NOT NULL,
-  `uploader_id` int(11) NOT NULL,
-  `uploader_name` varchar(100) NOT NULL,
-  `date` datetime DEFAULT CURRENT_TIMESTAMP,
-  `title` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  `status` enum('pending','failed','successful') DEFAULT 'pending',
-  `qualities` varchar(20) NOT NULL,
-  `duration` int(11) NOT NULL,
-  `thumbnails_count` int(11) NOT NULL,
-  `views` int(11) NOT NULL,
-  `comments` int(5) NOT NULL,
-  `state` enum('active','inactive','disabled') DEFAULT 'active',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `video_key` (`vkey`),
-  FULLTEXT KEY `title` (`title`,`description`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vkey` varchar(15) NOT NULL,
+  `userid` int(20) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `comment` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `videos`
+-- Dumping data for table `comments`
 --
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -56,4 +51,4 @@ CREATE TABLE `videos` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-10  0:11:06
+-- Dump completed on 2019-03-10  0:10:24
