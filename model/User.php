a7e54059ce2e1de15e01849288e10da1dd743e8f
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-03-12 17:46:39 +0500

    User class updated

--- model/User.php
+++ model/User.php
@@ -3,9 +3,48 @@
 /**
  * 
  */
-class User {
-	
-	function __construct($videoId) {
-		# code...
-	}
+class User extends Users {
+  
+  function __construct($user) {
+    $this->user = $user;
+    $this->data = array();
+  }
+
+  public function fetch() {
+    $this->initialize();
+    return $this->data = is_numeric($this->user) ? $this->getById($this->user, '*') : $this->getByUsername($this->user, '*');
+  }
+
+  public function name() {
+    return $this->data['username'];
+  }
+
+  public function userId() {
+    return $this->data['id'];
+  }
+
+  public function email() {
+    return $this->data['email'];
+  }
+
+  public function level() {
+    return $this->data['level'];
+  }
+
+  public function date() {
+    return $this->data['date'];
+  }
+
+  public function status() {
+    return $this->data['status'];
+  }
+
+  public function dob() {
+    return $this->data['dob'];
+  }
+
+  public function bio() {
+    return $this->data['bio'];
+  }
+
 }
