e2bb81b49c6afc783425e333943532bc1707e284
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-03-17 14:59:22 +0500

    Completed documentation for logs file

--- model/Logs.php
+++ model/Logs.php
@@ -1,19 +1,44 @@
 <?php
 
+/**
+* Name: Logs
+* Description: A class to create helpful logs
+* @author: Saqib Razzaq
+* @since: v1, Feburary, 2019
+* @link: https://github.com/briskLimbs/briskLimbs/blob/master/model/Logs.php
+*/
+
 class Logs {
-  
+  /*
+  * Holds path of file to write to
+  */
+  public $path;
+
   function __construct($path) {
     $this->path = $path;
   }
 
+  /**
+  * Create initial log data
+  * @return: { boolean }
+  */
   public function initialize() {
     return $this->write("Log started @ " . date("Y/m/d - H:i:s"));
   }
 
+  /**
+  * Check if a log file exists
+  * @return: { boolean }
+  */
   public function exists() {
     return file_exists($this->path);
   }
 
+  /**
+  * Write a new line to log
+  * @param: { $txt } { string } { text to write }
+  * @param: { $print } { boolean } { false by default, print and write log if true }
+  */
   public function write($txt, $print = false) {
     if ($print) {
       displayMessage($txt);
@@ -22,6 +47,10 @@ class Logs {
     return file_put_contents($this->path, $txt . PHP_EOL , FILE_APPEND | LOCK_EX);
   }
 
+  /**
+  * Reads a log file
+  * @return: { string }
+  */
   public function read() {
     return file_get_contents($this->path);
   }
