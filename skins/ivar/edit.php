d0f4db5b698e61a84ce7580f45551519f190cacc
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-03-25 01:08:41 +0500

    Finished tags and cats for frontend user

--- skins/ivar/edit.php
+++ skins/ivar/edit.php
@@ -7,24 +7,40 @@ if (!$users->authenticated()) {
 	jumpTo('home');
 }
 
-$key = $_GET['key'];
+$vkey = $_GET['key'];
 
 $videos = new Videos();
 $videos->initialize();
 
+$categories = new Categories();
+$activeCategories = array();
+foreach ($categories->listActive(20) as $key => $value) {
+	$activeCategories[$value['id']] = $value;
+}
+
 if (isset($_POST['title'])) {
-  if ($videos->update($key, $_POST)) {
+	$_POST['category'] = !empty($_POST['category']) ? implode(',', $_POST['category']) : 1;
+  if ($videos->update($vkey, $_POST)) {
     $parameters['message'] = 'Video updated successfully';
   } else {
     $data = $_POST;
   }
 }
 
+$data = empty($data) ? $videos->get($vkey) : $data;
+$cats = explode(',', $data['category']);
+foreach ($cats as $key => $cat) {
+	if (isset($activeCategories[$cat])) {
+		$data['categoryNames'][] = $activeCategories[$cat]['name'];
+	}
+}
+
 $parameters['_title'] = 'Edit - Dashboard';
 $parameters['_section'] = 'user_dashboard';
 $parameters['mainSection'] = 'edit';
 $parameters['dashSection'] = 'videos';
-$parameters['video'] = empty($data) ? $videos->get($key) : $data;
+$parameters['video'] = $data;
+$parameters['categories'] = $activeCategories;
 $parameters['_errors'] = $limbs->errors->collect();
 
 if (!$videos->validatePermissions($key)) {
