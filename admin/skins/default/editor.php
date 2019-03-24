770a13d2ed5f14989ffb89878f22fe0c643684f4
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-03-24 11:04:52 +0500

    Added code editing support, draft 1

--- admin/skins/default/editor.php
+++ admin/skins/default/editor.php
@@ -7,12 +7,41 @@ if (!$users->isAdmin()) {
 	jumpTo('home');
 }
 
+if (isset($_POST['list'])) {
+	$path = $_POST['list'];
+	if (file_exists($path)) {
+		if (is_dir($path)) {
+			$raw = glob("$path/*");
+			$list = array();
+			foreach ($raw as $key => $file) {
+				$list[basename($file)] = $file;
+			}
+			$response = array('status' => 'success', 'contents' => $list, 'code' => file_get_contents(current($list)));
+		} else {
+			$response = array('status' => 'error', 'contents' => false, 'message' => 'Invalid directory path');
+		}
+	} else {
+		$response = array('status' => 'error', 'contents' => false, 'message' => 'Path not found');
+	}
+
+	sendJsonResponse($response);
+}
+
 $section = isset($_GET['main']) ? $_GET['main'] : false;
 if ($section == 'addons' || $section == 'skins') {
 	$raw = glob(CORE_DIRECTORY . "/{$section}/*");
 	$items = array();
 	foreach ($raw as $key => $path) {
 		$items[basename($path)] = $path;
+	}
+
+	if (is_dir(current($items))) {
+		$path = current($items);
+		$subItems = array();
+		foreach (glob("$path/*") as $key => $path) {
+			$subItems[basename($path)] = $path;
+		}
+		$parameters['subItems'] = $subItems;
 	}
 }
 
