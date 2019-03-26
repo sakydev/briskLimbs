671ebd8cb2d5c33c7186c253ffa1ebc286e6b2ed
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-03-27 02:12:19 +0500

    Fixed: https://github.com/briskLimbs/briskLimbs/issues/6

--- admin/skins/default/index.php
+++ admin/skins/default/index.php
@@ -6,6 +6,15 @@ if (!$users->isAdmin()) {
 	jumpTo('home');
 }
 
+$videos = new Videos();
+$videos->initialize();
+
+$uploads = $listParameters = array();
+$listParameters['date'] = array('2019-03-04 00:00::00', '2019-03-04 23:59::59', 'between');
+
+$uploads['today'] = $videos->count($listParameters);
+$uploads['yesterday'] = $videos->count(array('date' => array(date("Y-m-d 00:00:00"), date("Y-m-d 00:00:00"), 'between')));
+
 $parameters['mainSection'] = 'dashboard';
 $parameters['_errors'] = $limbs->errors->collect();
 $parameters['_title'] = 'Admin Dashboard';
