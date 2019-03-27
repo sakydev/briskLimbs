75c298bc540a03d94c8a65bded2b99a636b5ae09
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-03-28 02:19:58 +0500

    Admin area categories php file added

---
+++ admin/skins/default/categories.php
@@ -0,0 +1,51 @@
+<?php
+
+global $limbs, $users;
+
+if (!$users->isAdmin()) {
+	jumpTo('home');
+}
+
+$categories = new Categories();
+
+if (isset($_POST['name'])) {
+	if ($categories->create($_POST)) {
+		$parameters['message'] = 'Category created successfully';
+	} else {
+		$parameters['category'] = $_POST;
+	}
+}
+
+if (isset($_GET['deactivate'])) {
+	if ($categories->deactivate($_GET['deactivate'])) {
+		$parameters['message'] = sprintf("Category %s deactivated successfully", $_GET['deactivate']);
+	}
+}
+
+if (isset($_GET['activate'])) {
+	if ($categories->activate($_GET['activate'])) {
+		$parameters['message'] = sprintf("Category %s activated successfully", $_GET['activate']);
+	}
+}
+
+if (isset($_GET['delete'])) {
+	if ($categories->delete($_GET['delete'])) {
+		$parameters['message'] = sprintf("Category %s deleted successfully", $_GET['delete']);
+	}
+}
+
+$list = isset($_GET['list']) ? $_GET['list'] : 'all';
+$subSection = !empty($_GET['list']) ? ucfirst($_GET['list']) : 'All';
+$listParameters = array();
+$listParameters['sort'] = 'id';
+$results = $categories->list($listParameters);
+
+$totalFound = count($results);
+$parameters['total'] = $total = $categories->count($listParameters);
+$parameters['results'] = $results;
+$parameters['mainSection'] = 'categories';
+$parameters['action'] = $_GET['action'];
+
+$parameters['_errors'] = $limbs->errors->collect();
+$parameters['_title'] = 'Categories Manager - Dashboard';
+$limbs->display('categories.html', $parameters);
