7e3a746efa803c3e3f2469adf045ad2d1c9eee08
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-04-06 18:47:27 +0500

    Initial dashboard stats

--- admin/skins/default/index.php
+++ admin/skins/default/index.php
@@ -10,12 +10,47 @@ $videos = new Videos();
 $videos->initialize();
 
 $uploads = $listParameters = array();
-$listParameters['date'] = array('2019-03-04 00:00::00', '2019-03-04 23:59::59', 'between');
 
+$yesterdayDate = date('Y-m-d', strtotime('-1 days'));
+$listParameters['date'] = array("$yesterdayDate  00:00::00", "$yesterdayDate 23:59::59", 'between');
+$uploads['yesterday'] = $videos->count($listParameters);
+
+$todayDate = date('Y-m-d');
+$listParameters['date'] = array("$todayDate  00:00::00", "$todayDate 23:59::59", 'between');
 $uploads['today'] = $videos->count($listParameters);
-$uploads['yesterday'] = $videos->count(array('date' => array(date("Y-m-d 00:00:00"), date("Y-m-d 00:00:00"), 'between')));
 
+$weekStart = date('Y-m-d', strtotime('-7 days'));
+$weekEnd = date('Y-m-d', time());
+$uploads['last_week'] = json_encode($limbs->database->rawQuery("SELECT DATE(date) AS label,COUNT(*) AS y FROM videos WHERE date BETWEEN '$weekStart' AND '$weekEnd' GROUP BY DATE(date)"));
+
+$weekEnd = $weekStart; // end where last week starts
+$weekStart = date('Y-m-d', strtotime('-14 days'));
+$uploads['before_last_week'] = json_encode($limbs->database->rawQuery("SELECT DATE(date) AS label,COUNT(*) AS y FROM videos WHERE date BETWEEN '$weekStart' AND '$weekEnd' GROUP BY DATE(date)"));
+
+$monthStart = date('Y-m-d', strtotime('-30 days'));
+$monthEnd = date('Y-m-d', time());
+
+$uploads['last_month'] = json_encode($limbs->database->rawQuery("SELECT DATE(date) AS label,COUNT(*) AS y FROM videos WHERE date BETWEEN '$monthStart' AND '$monthEnd' GROUP BY DATE(date)"));
+
+$monthEnd = $monthStart; // end where last week starts
+$monthStart = date('Y-m-d', strtotime('-60 days'));
+$uploads['before_last_month'] = json_encode($limbs->database->rawQuery("SELECT DATE(date) AS label,COUNT(*) AS y FROM videos WHERE date BETWEEN '$monthStart' AND '$monthEnd' GROUP BY DATE(date)"));
+
+$categories = new Categories();
+$catResults = json_encode($limbs->database->rawQuery("SELECT category,COUNT(*) AS total FROM videos GROUP BY category"));
+$uploads['categories'] = array();
+foreach ($catResults as $key => $cat) {
+	foreach (explode(',', $cat['category']) as $key => $category) {
+		$name = $categories->getName($category);
+		if (empty($name)) { continue; }
+		$uploads['categories'][$name] = isset($uploads['categories'][$name]) ? $uploads['categories'][$name] + $cat['total'] : $cat['total'];
+	}
+}
+
+$stats['uploads'] = $uploads;
 $parameters['mainSection'] = 'dashboard';
 $parameters['_errors'] = $limbs->errors->collect();
 $parameters['_title'] = 'Admin Dashboard';
+$parameters['stats'] = $stats;
+
 $limbs->display('dashboard.html', $parameters);
