c9dd5af0a8979942e0a18c42c805482d8a82a2b6
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-04-13 16:49:50 +0500

    Mail class completed sending mails

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 
 require_once 'vendor/autoload.php';
 require_once 'config.php';
+
 $request = array_filter(explode('/', trim($_SERVER['REQUEST_URI'])));
 $base = basename(CORE_DIRECTORY);
-$page = array_filter($request, function($c) use (&$base)
-{
-	if ($base != $c && !strstr($c, '?') && !strstr($c, '&'))
-    {
+$page = array_filter($request, function($c) use (&$base) {
+	if ($base != $c && !strstr($c, '?') && !strstr($c, '&')) {
 		return $c;
 	}
 });
@@ -15,11 +14,7 @@ $page = array_filter($request, function($c) use (&$base)
 define('IS_ADMIN', current($page) == 'admin' ? true : false);
 $page = current($page) == 'watch' ? 'watch' : end($page);
 
-global $database;
+global $database; // ah! just once
 $limbs = new Limbs($database);
-$mail = new Mail();
-$main->send('brisklimbs@gmail.com', 'Frank Castle', 'Oye zinda', 'This freaking shit');
-exit;
-
 $users = new Users();
 $limbs->stretch($page);
--- model/Mail.php
+++ model/Mail.php
@@ -12,14 +12,22 @@ class Mail {
 
     $this->mailer = new PHPMailer();
     $this->mailer->isSMTP();
-    $this->mailer->SMTPAuth = true;
     $this->mailer->CharSet = 'utf-8';
+    $this->mailer->Host = $this->settings->get('mailer_host');
+    $this->mailer->Port = $this->settings->get('mailer_port');
+    $this->mailer->SMTPSecure = $this->settings->get('mailer_smtp_secure');
+    $this->mailer->SMTPAuth = true;
+    $this->mailer->AuthType = $this->settings->get('mailer_authtype');
+    $this->mailer->SMTPDebug = false; // use 1 or 2 when testing
   }
 
   public function send($recipientMail, $recipientName, $subject, $message) {
+  	$clientId = $this->settings->get('mailer_clientid');
+  	$clientSecret = $this->settings->get('mailer_clientsecret');
+
 		$provider = new Google([
-			'clientId' => $this->settings->get('mailer_clientid'), 
-			'clientSecret' => $this->settings->get('mailer_clientsecret'),
+			'clientId' => $clientId, 
+			'clientSecret' => $clientSecret,
 		]);
 		$this->mailer->setOAuth(
 		    new OAuth([
