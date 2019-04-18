70699be67cf2235c6973e0a431ab5814602f29b2
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-04-18 16:29:55 +0500

    Code style changes

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 ini_set('display_errors', 'On');
 error_reporting(-1);
 
-if (!file_exists(__DIR__ . '/configs/db.php'))
-{
-    header("Location: installer/install.php");
-    exit;
+if (!file_exists(__DIR__ . '/configs/db.php')) {
+  header("Location: installer/install.php");
+  exit;
 }
+
 require 'configs/constants.php';
 require 'configs/db.php';
 require 'helpers/devFunctions.php';
