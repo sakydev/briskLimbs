476ae997cd8fb7c996aaaf20b95edd39939ec57d
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-04-18 16:28:46 +0500

    Reverted code style changes

--- config.php
+++ config.php
@@ -14,26 +14,19 @@ require 'helpers/devFunctions.php';
 require 'helpers/videoFunctions.php';
 require 'helpers/functions.php';
 
-spl_autoload_register(function ($class)
-{
-    $class = ucfirst($class);
-    if (file_exists(MODEL_DIRECTORY . '/' . $class . '.php'))
-    {
-        require_once MODEL_DIRECTORY . '/' . $class . '.php';
+spl_autoload_register(function ($class) {
+  $class = ucfirst($class);
+  if (file_exists(MODEL_DIRECTORY . '/' . $class . '.php')) {
+    require_once MODEL_DIRECTORY . '/' . $class . '.php';
+  } else {
+    $class = strtolower($class);
+    if (file_exists(HELPERS_DIRECTORY . '/' . $class . '.php')) {
+      require_once HELPERS_DIRECTORY . '/' . $class . '.php';
+    } else {
+      exit(MODEL_DIRECTORY . '/' . $class . '.php');
+      exit('Error Loading Class File |' . $class . '| core:' . CORE_DIRECTORY);
     }
-    else
-    {
-        $class = strtolower($class);
-        if (file_exists(HELPERS_DIRECTORY . '/' . $class . '.php'))
-        {
-            require_once HELPERS_DIRECTORY . '/' . $class . '.php';
-        }
-        else
-        {
-            exit(MODEL_DIRECTORY . '/' . $class . '.php');
-            exit('Error Loading Class File |' . $class . '| core:' . CORE_DIRECTORY);
-        }
-    }
+  }
 });
 
 session_start();
