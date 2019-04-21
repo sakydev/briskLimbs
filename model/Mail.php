4430cf5337096d8ddb3a12c45b6eb88bba2d88e2
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-04-21 09:51:15 +0500

    SMPT email support added

--- model/Mail.php
+++ model/Mail.php
@@ -16,39 +16,17 @@ class Mail {
     $this->mailer->Host = $this->settings->get('mailer_host');
     $this->mailer->Port = $this->settings->get('mailer_port');
     $this->mailer->SMTPSecure = $this->settings->get('mailer_smtp_secure');
+    $this->mailer->Username = $this->settings->get('mailer_smpt_username');
+    $this->mailer->Password = $this->settings->get('mailer_smpt_password');;
     $this->mailer->SMTPAuth = true;
-    $this->mailer->AuthType = $this->settings->get('mailer_authtype');
     $this->mailer->SMTPDebug = false; // use 1 or 2 when testing
   }
 
   public function send($subject, $message, $recipientMail, $recipientName) {
-  	$clientId = $this->settings->get('mailer_clientid');
-  	$clientSecret = $this->settings->get('mailer_clientsecret');
-
-		$provider = new Google([
-			'clientId' => $clientId, 
-			'clientSecret' => $clientSecret,
-		]);
-		$this->mailer->setOAuth(
-		    new OAuth([
-          'provider' => $provider,
-          'clientId' => $clientId,
-          'clientSecret' => $clientSecret,
-          'refreshToken' => $this->settings->get('mailer_refreshtoken'),
-          'userName' => $this->settings->get('mailer_sender_email'),
-	        ]
-		    )
-		);
-
 		$this->mailer->setFrom($this->settings->get('mailer_sender_email'), $this->settings->get('mailer_sender_name'));
 		$this->mailer->addAddress($recipientMail, $recipientName);
 		$this->mailer->Subject = $subject;
 		$this->mailer->msgHTML($message);
-		//send the message, check for errors
-		if ($this->mailer->send()) {
-		  return true;
-		} else {
-		  $this->errors->add($this->mailer->ErrorInfo);
-		}
+		return $this->mailer->send() ? true : $this->errors->add($this->mailer->ErrorInfo);
   }
 }
