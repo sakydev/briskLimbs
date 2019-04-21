38bafc9d157b6ba6c07b3b0d2d3dfa9608549d8c
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-04-21 17:12:31 +0500

    Fixed: https://github.com/briskLimbs/briskLimbs/issues/27

---
+++ admin/skins/default/edituser.php
@@ -0,0 +1,23 @@
+<?php
+
+global $limbs, $users;
+$settings = $limbs->settings;
+
+if (!$users->isAdmin()) {
+	jumpTo('home');
+}
+
+$username = $_GET['username'];
+if (isset($_POST['email'])) {
+  if ($users->update($username, $_POST)) {
+    $parameters['message'] = 'User updated successfully';
+  } else {
+    $data = $_POST;
+  }
+}
+
+$parameters['_title'] = 'Edit User - Dashboard';
+$parameters['mainSection'] = 'users';
+$parameters['user'] = empty($data) ? $users->get($username) : $data;
+$parameters['_errors'] = $limbs->errors->collect();
+$limbs->display('edituser.html', $parameters);
