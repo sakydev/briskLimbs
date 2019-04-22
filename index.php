fd23cb8c9024bff5bc099fe62c500536964767cd
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-04-22 13:45:47 +0500

    Added error for missing dependencies

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-require_once 'vendor/autoload.php';
+$vendorFile = 'vendor/autoload.php';
+if (!file_exists($vendorFile)) {
+	exit("BriskLimbs dependencies are missing. Please make sure to run [composer install]");
+}
+
+require_once $vendorFile;
 require_once 'config.php';
 
 $request = array_filter(explode('/', trim($_SERVER['REQUEST_URI'])));
