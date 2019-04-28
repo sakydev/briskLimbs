0d90747c6137446340a085446fbcdd28ff98a03c
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-04-28 09:55:09 +0500

    Replace pipes with slash

--- addons/developerTools/developerTools.php
+++ addons/developerTools/developerTools.php
@@ -5,14 +5,14 @@ $addons = new Addons();
 
 define('DEV_CORE_PATH', __DIR__);
 define('DEV_CORE_NAME', basename(DEV_CORE_PATH));
-$pages = DEV_CORE_NAME . '|pages';
+$pages = DEV_CORE_NAME . '/pages';
 $menu = array(
   'developer_tools' => array(
     'display_name' => 'Developer Tools',
     'sub' => array(
-      'PHP Info' => $pages . '|info.php',
-      'Requirements' => $pages . '|requirements.php',
-      'Server Configs' => $pages . '|configs.php'
+      'PHP Info' => $pages . '/info.php',
+      'Requirements' => $pages . '/requirements.php',
+      'Server Configs' => $pages . '/configs.php'
     )
   )
 );
