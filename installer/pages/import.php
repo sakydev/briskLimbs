4e12269bed12965b8753971345c6ce32627d1d88
Saqib Razzaq <saqibrzzaq@gmail.com>
2021-05-26 15:15:40 +0500

    Keep username and database info on error to avoid retyping

--- installer/pages/import.php
+++ installer/pages/import.php
@@ -1,5 +1,6 @@
 <?php
 
+$username = $password = $database = '';
 if (isset($_POST['username'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
@@ -28,11 +29,11 @@ if (isset($_POST['username'])) {
     <?php endif; ?>
     <h1 class="h3 mb-3 font-weight-normal">Database Details</h1>
     <label for="username" class="float-left mt-2">Database Username</label>
-    <input type="user" name="username" class="form-control" placeholder="Database username here" required autofocus>
+    <input type="user" name="username" class="form-control" placeholder="Database username here" value="<?= $username ?>" required autofocus>
     <label for="password" class="float-left mt-2">Database Password</label>
     <input type="password" name="password" class="form-control" placeholder="Database password here">
     <label for="database" class="float-left mt-2">Database Name</label>
-    <input type="database" name="database" class="form-control" placeholder="Database name here" required>
+    <input type="database" name="database" class="form-control" placeholder="Database name here" value="<?= $database ?>" required>
     <button class="btn btn-primary mt-2" type="submit" value="install">Install</button>
   </form>
 </body>
