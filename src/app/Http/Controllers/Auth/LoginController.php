d755b3a219f13ca66aeebd61dace0c8f8dd4d503
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-21 11:31:04 +0500

    BRIS-3: complete login page

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,25 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('username', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('home');
+        }
+
+        return redirect("login")->withErrors('You have entered invalid credentials');
     }
 }
--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -7,30 +7,45 @@
                 <div class="col-lg">
                     <div class="container-tight">
                         <div class="text-center mb-4">
-                            <a href="." class="navbar-brand navbar-brand-autodark"><img src="{{ asset('_tabler/static/logo.svg') }}" height="36" alt=""></a>
+                            <a href="." class="navbar-brand navbar-brand-autodark"><img
+                                    src="{{ asset('_tabler/static/logo.svg') }}" height="36" alt=""></a>
                         </div>
+                        @include('components.errors')
                         <div class="card card-md">
                             <div class="card-body">
                                 <h2 class="h2 text-center mb-4">Login to your account</h2>
-                                <form action="./" method="get" autocomplete="off" novalidate>
+                                <form method="POST" action="{{ route('login') }}" autocomplete="off">
+                                    @csrf
                                     <div class="mb-3">
-                                        <label class="form-label">Email address</label>
-                                        <input type="email" class="form-control" placeholder="your@email.com" autocomplete="off">
+                                        <label class="form-label">Username</label>
+                                        <input type="text" name="username" class="form-control" placeholder="Jon"
+                                               required>
+                                        @error('username')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
                                     <div class="mb-2">
                                         <label class="form-label">
                                             Password
                                             <span class="form-label-description">
-                          <a href="./forgot-password.html">I forgot password</a>
-                        </span>
+                                                <a href="./forgot-password.html">I forgot password</a>
+                                            </span>
                                         </label>
                                         <div class="input-group input-group-flat">
-                                            <input type="password" class="form-control"  placeholder="Your password"  autocomplete="off">
+                                            <input type="password" name="password" class="form-control"
+                                                   placeholder="Your password" autocomplete="off" required>
                                             <span class="input-group-text">
-                          <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip">
-                            @include('svg.icons.eye')
-                          </a>
-                        </span>
+                                                <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip">
+                                                    @include('svg.icons.eye')
+                                                </a>
+                                            </span>
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                         </div>
                                     </div>
                                     <div class="mb-2">
@@ -69,12 +84,13 @@
                             </div>
                         </div>
                         <div class="text-center text-muted mt-3">
-                            Don't have account yet? <a href="./sign-up.html" tabindex="-1">Sign up</a>
+                            Don't have account yet? <a href="{{ route('register') }}" tabindex="-1">Register</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg d-none d-lg-block">
-                    <img src="{{ asset('_tabler/static/illustrations/undraw_secure_login_pdn4.svg') }}" height="300" class="d-block mx-auto" alt="">
+                    <img src="{{ asset('_tabler/static/illustrations/undraw_secure_login_pdn4.svg') }}" height="300"
+                         class="d-block mx-auto" alt="">
                 </div>
             </div>
         </div>
