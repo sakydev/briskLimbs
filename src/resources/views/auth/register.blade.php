38f3d1fe4da540fb010826fbaac950392812f6c7
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-21 18:16:54 +0500

    BRIS-3: restore signup page

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -6,27 +6,56 @@
             <div class="text-center mb-4">
                 <a href="." class="navbar-brand navbar-brand-autodark"><img src="{{ asset("_tabler/static/logo.svg") }}" height="36" alt=""></a>
             </div>
-            <form class="card card-md" method="POST" action="{{ route('password.email') }}"">
+            <form class="card card-md" action="{{ route("register") }}" method="POST">
                 @csrf
                 <div class="card-body">
-                    <h2 class="card-title text-center mb-4">Reset password</h2>
+                    <h2 class="card-title text-center mb-4">Create new account</h2>
+                    <div class="mb-3">
+                        <label class="form-label">Username</label>
+                        <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" placeholder="Enter name" value="{{ old('name') }}" required>
+                        @error('username')
+                            <div class="invalid-feedback">
+                                <strong>{{ $message }}</strong>
+                            </div>
+                        @enderror
+                    </div>
                     <div class="mb-3">
                         <label class="form-label">Email address</label>
-                        <input type="email" name="email" class="form-control @if ($errors->has('email')) is-invalid @endif" placeholder="snow@thewall.com" value="{{ old('email') }}" required>
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" placeholder="Enter email" required>
                         @error('email')
-                        <div class="invalid-feedback">
-                            <strong>{{ $message }}</strong>
-                        </div>
+                            <div class="invalid-feedback">
+                                <strong>{{ $message }}</strong>
+                            </div>
                         @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <div class="input-group input-group-flat">
+                            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"  placeholder="Password"  autocomplete="off" required>
+                            @error('password')
+                                <div class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-check">
+                            <input type="checkbox" class="form-check-input"/>
+                            <span class="form-check-label">Agree the <a href="./terms-of-service.html" tabindex="-1">terms and policy</a>.</span>
+                        </label>
                     </div>
                     <div class="form-footer">
                         @include('components.button', [
                             'modifier_class' => 'btn btn-primary w-100',
-                            'text' => __('Send Password Reset Link'),
+                            'text' => 'Create new account',
                         ])
                     </div>
                 </div>
             </form>
+            <div class="text-center text-muted mt-3">
+                Already have account? <a href="{{ route("login") }}" tabindex="-1">Login</a>
+            </div>
         </div>
     </div>
 @endsection
