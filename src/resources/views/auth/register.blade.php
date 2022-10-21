eee0ba107a1515e2746eee29eb0f3f77c6414856
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-21 12:07:20 +0500

    BRIS-3: update laravel core blade files to use theme layout

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -6,19 +6,10 @@
             <div class="text-center mb-4">
                 <a href="." class="navbar-brand navbar-brand-autodark"><img src="{{ asset("_tabler/static/logo.svg") }}" height="36" alt=""></a>
             </div>
-            <form class="card card-md" action="{{ route('register') }}" method="POST" autocomplete="off">
+            <form class="card card-md" method="POST" action="{{ route('password.email') }}"">
                 @csrf
                 <div class="card-body">
-                    <h2 class="card-title text-center mb-4">Create new account</h2>
-                    <div class="mb-3">
-                        <label class="form-label">Username</label>
-                        <input type="text" name="username" class="form-control @if ($errors->has('username')) is-invalid @endif" placeholder="Jon Snow" value="{{ old('username') }}" required>
-                        @error('username')
-                        <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                    <h2 class="card-title text-center mb-4">Reset password</h2>
                     <div class="mb-3">
                         <label class="form-label">Email address</label>
                         <input type="email" name="email" class="form-control @if ($errors->has('email')) is-invalid @endif" placeholder="snow@thewall.com" value="{{ old('email') }}" required>
@@ -28,34 +19,14 @@
                         </div>
                         @enderror
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label">Password</label>
-                        <div class="input-group input-group-flat">
-                            <input type="password" name="password" class="form-control @if ($errors->has('password')) is-invalid @endif"  placeholder="****"  autocomplete="off" required>
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-check">
-                            <input type="checkbox" class="form-check-input"/>
-                            <span class="form-check-label">Agree the <a href="./terms-of-service.html" tabindex="-1">terms and policy</a>.</span>
-                        </label>
-                    </div>
                     <div class="form-footer">
                         @include('components.button', [
                             'modifier_class' => 'btn btn-primary w-100',
-                            'text' => 'Create new account',
+                            'text' => __('Send Password Reset Link'),
                         ])
                     </div>
                 </div>
             </form>
-            <div class="text-center text-muted mt-3">
-                Already have account? <a href="{{ route('login') }}" tabindex="-1">Login</a>
-            </div>
         </div>
     </div>
 @endsection
