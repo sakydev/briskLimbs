1225ae8d3c9624c07a70601b08859bee9706ca19
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-21 07:45:09 +0500

    skeleton: add home page skeleton

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home');
 
 Route::view('/blank', 'blank');
 Route::view('/login', 'login');
