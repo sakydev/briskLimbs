8ad5899759abcc53239ae137ead976bfbb282a8b
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-22 18:51:07 +0500

    BRIS-4: add `sendErrorJsonResponse` to `Controller`

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function sendErrorJsonResponse(mixed $data): JsonResponse
+    {
+        return new JsonResponse($data);
+    }
 }
