ebc55654281e4d50a3f9257a395ef35c48fdd0e0
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-22 08:58:01 +0500

    BRIS-15: terms seeder added

---
+++ src/database/seeders/TermsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Page;
+
+class TermsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Page::create([
+            'title' => 'Terms and policy',
+            'slug' => 'terms',
+            'content' => '<h1>Sample Headingx</h1><p>Update this page to include your own terms</p>',
+        ]);
+    }
+}
