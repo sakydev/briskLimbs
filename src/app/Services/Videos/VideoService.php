fe5963a1c587de057cc738c4aab19c1ba55e5718
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-22 18:51:58 +0500

    BRIS-4: add `validateCanUpload` and `validateRequest` to `VideoValidationService`

---
+++ src/app/Services/Videos/VideoService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\Videos;
+
+class VideoService
+{
+
+}
---
+++ src/app/Services/Videos/VideoValidationService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services\Videos;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class VideoValidationService
+{
+    public function validateCanUpload(User $user): ?array
+    {
+        $errors = [];
+        if ($user->status !== User::ACTIVE_STATE) {
+            return $errors[] = [
+                'title' => 'User must be active',
+                'description' => 'User must be active in order to upload videos',
+            ];
+        }
+    }
+
+    public function validateRequest(array $input): ?array
+    {
+        $rules = [
+            'file' => [
+                'required',
+                'mimes:' . env('support_video_formats'),
+                'max:' . env('max_video_upload_size'),
+
+            ],
+            'title' => ['required', 'string', 'min:10', 'max:100'],
+            'description' => ['required', 'string', 'min:10', 'max:3000'],
+        ];
+
+        $validator = Validator::make($input, $rules);
+        $errors = [];
+        if ($validator->fails()) {
+            foreach ($validator->messages()->get('*') as $title => $description) {
+                $errors[] = [
+                    'title' => $title,
+                    'description' => current($description),
+                ];
+            }
+
+            return $errors;
+        }
+
+        return null;
+    }
+}
