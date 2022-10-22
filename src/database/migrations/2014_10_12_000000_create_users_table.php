248cf39cceb463f08040a6af010ab2aef6c08e2e
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-22 18:50:39 +0500

    BRIS-4: add `status` and `level` fields to user table

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
+            $table->string('status'); // active, inactive
+            $table->integer('level'); // 1-5
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
