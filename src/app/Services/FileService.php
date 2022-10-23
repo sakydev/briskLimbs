72c0247434e8905099d95a48893106f3cd33644d
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-23 11:19:54 +0500

    BRIS-4: refactor `FileService` for clarity

--- src/app/Services/FileService.php
+++ src/app/Services/FileService.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Storage;
 
 abstract class FileService
 {
+    private const TEMPORARY_DIRECTORY = 'temporary';
+    private const VIDEOS_DIRECTORY = 'videos';
+
     public abstract function store(UploadedFile $file, string $filename): string;
 
     public function getDisk(): Filesystem
@@ -19,10 +22,7 @@ abstract class FileService
     }
     public function createTemporaryDirectories(): bool
     {
-        return $this->makeDirectory('temporary/videos')
-        && $this->makeDirectory('temporary/thumbnails')
-        && $this->makeDirectory('temporary/avatars')
-        && $this->makeDirectory('temporary/covers');
+        return $this->makeDirectory(self::TEMPORARY_DIRECTORY . '/' . self::VIDEOS_DIRECTORY);
     }
 
     public function makeDirectory(string $directory): bool
