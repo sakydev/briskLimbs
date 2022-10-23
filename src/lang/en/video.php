648de2930f42e38a4f2437eba8c4aedae60ddcc8
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-23 15:07:51 +0500

    BRIS-4: translation files added

---
+++ src/lang/en/video.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'errors' => [
+        'failed_upload' => 'Failed to upload video to server',
+        'failed_upload_permissions' => 'User has insufficient permissions to upload',
+    ],
+    'success_video_save' => 'Video saved successfully.',
+    'success_video_update' => 'Video updated successfully.',
+];
