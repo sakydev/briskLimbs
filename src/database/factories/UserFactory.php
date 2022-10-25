a54c1b41ede384edf5cceb22748a2a7df38feb97
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-25 06:58:43 +0500

    BRIS-16: default users seeder

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -18,8 +18,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
+            'status' => 'active',
+            'level' => 5,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
