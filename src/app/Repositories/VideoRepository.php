abe651b08c1e02d9c069fe277f58afc0e2166ddf
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-26 20:31:34 +0500

    BRIS-5: refactor video repository

--- src/app/Repositories/VideoRepository.php
+++ src/app/Repositories/VideoRepository.php
@@ -6,7 +6,7 @@ use App\Models\Video;
 
 class VideoRepository
 {
-    public function getById(int $videoId): ?Video
+    public function get(int $videoId): ?Video
     {
         return (new Video())->where('id', $videoId)->first();
     }
@@ -20,8 +20,24 @@ class VideoRepository
         return (new Video())->create($input);
     }
 
-    public function updateById(array $input, int $videoId): ?int
+    public function update(array $fieldValueParis, int|Video $video): ?int
     {
-        return (new Video())->where('id', $videoId)->update($input);
+        if ($video instanceof Video) {
+            return $video->update($fieldValueParis);
+        }
+
+        return (new Video())->where('id', $video)->update($fieldValueParis);
+    }
+
+    public function updateStatus(string $status, int|Video $video): bool
+    {
+        $fieldValueParis = ['status' => $status];
+        return $this->update($fieldValueParis, $video);
+    }
+
+    public function updateState(string $state, int|Video $video): bool
+    {
+        $fieldValueParis = ['state' => $state];
+        return $this->update($fieldValueParis, $video);
     }
 }
