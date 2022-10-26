35c7812c2c0afb716863d8128d5296f35d2c99cb
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-26 20:26:17 +0500

    BRIS-5: add `status` constants to Video model

--- src/app/Models/Video.php
+++ src/app/Models/Video.php
@@ -10,4 +10,9 @@ class Video extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public const VIDEO_PROCESSING_PENDING = 'pending';
+    public const VIDEO_PROCESSING_PROGRESS = 'progress';
+    public const VIDEO_PROCESSING_SUCCESS = 'success';
+    public const VIDEO_PROCESSING_FAILED = 'failure';
 }
