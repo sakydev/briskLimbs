94660751c3a1cfd499c11897ad7fc9f85b6dc4e1
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-29 16:43:57 +0500

    BRIS-5: remove method no longer required

--- src/app/Services/UploadService.php
+++ src/app/Services/UploadService.php
@@ -8,9 +8,4 @@ use Illuminate\Support\Facades\Storage;
 abstract class UploadService
 {
     public abstract function store(UploadedFile $file, string $filename): ?string;
-
-    public function createTemporaryDirectories(): bool
-    {
-        return Storage::disk('local')->makeDirectory(config('paths.temporary_videos'));
-    }
 }
