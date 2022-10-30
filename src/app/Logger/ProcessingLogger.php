c788813c53a6c4ae0da3aff0721a43cb54aa8e65
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-30 07:49:21 +0500

    BRIS-5: base `ProcessingLogger` completed

--- src/app/Logger/ProcessingLogger.php
+++ src/app/Logger/ProcessingLogger.php
@@ -2,43 +2,61 @@
 
 namespace App\Logger;
 
+use App\Services\FileService;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 class ProcessingLogger
 {
+    private string $filename;
+    private Logger $log;
+
     public function __construct(string $filename) {
         $this->filename = $filename;
         $this->log = new Logger($filename);
         $this->log->pushHandler(
-            new StreamHandler(
-                storage_path('logs/' . $filename . '.log')
-            ), Logger::INFO
+            new StreamHandler(FileService::getLog($filename)), Logger::INFO
         );
     }
 
     public function debug(string $message): void
     {
-        $this->log->debug($message);
+        $this->log->debug("$this->filename => $message");
     }
 
     public function info(string $message): void
     {
-        $this->log->info($message);
+        $this->log->info("$this->filename => $message");
     }
 
     public function notice(string $message): void
     {
-        $this->log->notice($message);
+        $this->log->notice("$this->filename => $message");
     }
 
     public function warning(string $message): void
     {
-        $this->log->warning($message);
+        $this->log->warning("$this->filename => $message");
     }
 
     public function error(string $message): void
     {
-        $this->log->error($message);
+        $this->log->error("$this->filename => $message");
+    }
+
+    public function statusChange(string $status): void {
+        $this->info("set video status => $status");
+    }
+
+    public function inputFile(string $input): void {
+        $this->info("set input file => $input");
+    }
+
+    public function processedThumbnails(array $thumbnails, int $quality): void {
+        $this->info("processed $quality thumbnails successfully => " . implode(',', $thumbnails));
+    }
+
+    public function processedVideos(array $videos, int $quality): void {
+        $this->info("processed $quality videos successfully => " . implode(',', $videos));
     }
 }
