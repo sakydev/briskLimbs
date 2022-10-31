2fcd3d8ae3b113e0e4e3b3e166a0dea7ccfaee26
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-31 07:44:14 +0500

    BRIS-19: lang strings added

--- src/lang/en/user.php
+++ src/lang/en/user.php
@@ -4,6 +4,9 @@ return [
     'errors' => [
         'restricted_registrations' => 'Registrations are restricted at the moment',
         'failed_insert' => 'User database insert failed',
+        'not_found' => 'User not found',
     ],
     'registration_success' => 'User created successfully',
+    'login_success' => 'User logged in successfully',
+    'fetch_success' => 'User fetched successfully',
 ];
