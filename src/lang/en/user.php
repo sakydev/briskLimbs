b9a7753c3ad22e2e2106ddb36db7a7ba089aa2e8
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-10-31 09:36:50 +0500

    BRIS-20: add update related lang strings

--- src/lang/en/user.php
+++ src/lang/en/user.php
@@ -4,9 +4,12 @@ return [
     'errors' => [
         'failed_registration_restriction' => 'Registrations are restricted at the moment',
         'failed_insert' => 'User database insert failed',
+        'failed_update' => 'User failed to update',
         'failed_find' => 'User not found',
+        'failed_update_permissions' => 'User has insufficient permissions to update',
     ],
     'success_registration' => 'User created successfully',
     'success_login' => 'User logged in successfully',
     'success_find' => 'User fetched successfully',
+    'success_update' => 'User updated successfully',
 ];
