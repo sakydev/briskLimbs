fa4d805fd4ab774c8133710cb278c1733ace7370
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-11-02 09:49:55 +0500

    BRIS-23: add `testUserCanListUsers` test

---
+++ src/tests/Feature/Api/V1/UserControllerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature\Api\V1;
+
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserControllerTest extends TestCase {
+    use RefreshDatabase;
+
+    private const BASE_URL = 'api/V1/users';
+
+    private User $adminUser;
+    private User $basicUser;
+    private User $inactiveUser;
+
+    private const USER_LIST_SUCCESSFUL_RESPONSE_STRUCTURE = [
+        'success',
+        'messages',
+        'data' => [
+            '*' => [
+                'id',
+                'username',
+                'email',
+                'status',
+                'created_at',
+                'updated_at',
+            ],
+        ],
+    ];
+
+    public function setUp(): void {
+        parent::setUp();
+
+        $this->adminUser = $this->createAdminUser('daemon');
+        $this->basicUser = $this->createBasicUser('tully');
+        $this->inactiveUser = $this->createBasicInactiveUser('ned');
+    }
+
+    /**
+     * @dataProvider usersCanListUsersDataProvider
+     */
+    public function testUserCanListUsers(
+        string $userProperty,
+        int $expectedStatus,
+        ?array $expectedJSONStructure,
+    ) {
+        $this->be($this->$userProperty);
+
+        $response = $this->getJson(self::BASE_URL);
+        $response->assertStatus($expectedStatus);
+
+        if ($expectedJSONStructure) {
+            $response->assertJsonStructure($expectedJSONStructure);
+        }
+    }
+
+    public function usersCanListUsersDataProvider(): array
+    {
+        return [
+            'Admin User' => [
+                'user' => 'adminUser',
+                'expectedStatus' => Response::HTTP_OK,
+                'expectedJsonStructure' => self::USER_LIST_SUCCESSFUL_RESPONSE_STRUCTURE,
+            ],
+            'Basic user' => [
+                'user' => 'basicUser',
+                'expectedStatus' => Response::HTTP_OK,
+                'expectedJsonStructure' => self::USER_LIST_SUCCESSFUL_RESPONSE_STRUCTURE,
+            ],
+            'Inactive user' => [
+                'user' => 'inactiveUser',
+                'expectedStatus' => Response::HTTP_FORBIDDEN,
+                'expectedJsonStructure' => null,
+            ],
+        ];
+    }
+}
