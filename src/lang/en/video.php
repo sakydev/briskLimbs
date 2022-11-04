a5d6a123b85b535d70113dc58df5cee9e33d7638
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-11-04 10:11:11 +0500

    BRIS-35: add activate/deactivate translations

--- src/lang/en/video.php
+++ src/lang/en/video.php
@@ -8,8 +8,12 @@ return [
         'failed_upload_permissions' => 'User has insufficient permissions to upload',
         'failed_update_permissions' => 'User has insufficient permissions to update',
         'failed_meta_extraction' => 'Failed to decode video meta data',
+        'failed_already_activate' => 'Video is already active',
+        'failed_already_deactivate' => 'Video is already inactive',
     ],
     'success_save' => 'Video saved successfully.',
     'success_update' => 'Video updated successfully.',
     'success_list' => 'Videos fetched successfully.',
+    'success_activate' => 'Video activated successfully.',
+    'success_deactivate' => 'Video deactivated successfully.',
 ];
