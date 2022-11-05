40af1a123fbcb626f5e25641eb81894938458b1f
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-11-05 14:58:43 +0500

    BRIS-47: add `list` method to PageRepository

--- src/app/Repositories/PageRepository.php
+++ src/app/Repositories/PageRepository.php
@@ -3,12 +3,24 @@
 namespace App\Repositories;
 
 use App\Models\Page;
+use Illuminate\Database\Eloquent\Collection;
 
 class PageRepository
 {
     public function getBySlug(string $slug): ?Page
     {
         return (new Page())->where('slug', $slug)->first();
+    }
+
+    public function list(array $parameters, int $page, int $limit): Collection {
+        $skip = ($page * $limit) - $limit;
+
+        $pages = new Page();
+        foreach ($parameters as $name => $value) {
+            $pages = $pages->where($name, $value);
+        }
+
+        return $pages->skip($skip)->take($limit)->orderBy('id', 'DESC')->get();
     }
 
     public function create(array $input): Page
