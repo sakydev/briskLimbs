5a78c2cdb4dbebb9b7d9e6150b7aa857a8ac2ea4
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-11-06 11:26:56 +0500

    BRIS-47: add page publish, unpublish and update validations

--- src/app/Services/PageValidationService.php
+++ src/app/Services/PageValidationService.php
@@ -2,12 +2,24 @@
 
 namespace App\Services;
 
+use App\Models\Page;
 use App\Models\User;
 use Symfony\Component\HttpFoundation\Response;
 
 class PageValidationService extends ValidationService
 {
     public function validateCanCreate(User $user): bool {
+        if (!$user->isAdmin()) {
+            $this->addError(__('page.failed.store.permissions'));
+            $this->setStatus(Response::HTTP_FORBIDDEN);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function validateCanUpdate(User $user): bool {
         if (!$user->isAdmin()) {
             $this->addError(__('page.failed.update.permissions'));
             $this->setStatus(Response::HTTP_FORBIDDEN);
@@ -16,6 +28,65 @@ class PageValidationService extends ValidationService
         }
 
         return true;
+    }
+
+    public function validateCanDelete(User $user): bool {
+        if (!$user->isAdmin()) {
+            $this->addError(__('page.failed.delete.permissions'));
+            $this->setStatus(Response::HTTP_FORBIDDEN);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function validateAlreadyPublished(Page $page): bool {
+        if ($page->isPublished()) {
+            $this->addError(__('page.failed.update.already_published'));
+            $this->setStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function validateAlreadyUnpublished(Page $page): bool {
+        if (!$page->isPublished()) {
+            $this->addError(__('page.failed.update.already_unpublished'));
+            $this->setStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function validatePreConditionsToPublish(Page $page, User $user): void {
+        $this->resetErrors();
+
+
+        if (!$this->validateCanUpdate($user)) {
+            return;
+        }
+
+        if (!$this->validateAlreadyPublished($page)) {
+            return;
+        }
+    }
+
+    public function validatePreConditionsToUnpublish(Page $page, User $user): void {
+        $this->resetErrors();
+
+
+        if (!$this->validateCanUpdate($user)) {
+            return;
+        }
+
+        if (!$this->validateAlreadyUnpublished($page)) {
+            return;
+        }
     }
 
     public function validateCreateRequest(array $input): ?array {
