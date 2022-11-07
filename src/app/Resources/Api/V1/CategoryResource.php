a21e94c68ebeedaf6d1c5451e5787caa56a4c740
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-11-07 10:45:07 +0500

    BRIS-35: add CategoryResource

---
+++ src/app/Resources/Api/V1/CategoryResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Resources\Api\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request = []): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'state' => $this->state,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
