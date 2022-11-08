a3c08e49022089336ab6ade138cc2401edd16844
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-11-08 07:01:08 +0500

    BRIS-34: add basic video search

---
+++ src/app/Http/Controllers/Api/V1/Search/VideoSearchController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Search;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\VideoRepository;
+use App\Resources\Api\V1\ErrorResponse;
+use App\Resources\Api\V1\SuccessResponse;
+use App\Resources\Api\V1\VideoResource;
+use App\Services\Videos\VideoValidationService;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VideoSearchController extends Controller
+{
+    public function __construct(
+      private VideoValidationService $videoValidationService,
+      private VideoRepository $videoRepository,
+    ) {}
+
+    public function search(Request $request): SuccessResponse|ErrorResponse {
+        $input = $request->only('query');
+        $query = $request->get('query');
+
+        $searchErrors = $this->videoValidationService->validateSearchRequest($input);
+        if ($searchErrors) {
+            return new ErrorResponse(
+                $searchErrors,
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        $videos = VideoResource::collection(
+            $this->videoRepository->search(
+                $query,
+                $request->get('page', 1),
+                $request->get('limit', 10),
+            ),
+        );
+
+        return new SuccessResponse(
+            __('video.success_list'),
+            $videos->toArray($request),
+            Response::HTTP_OK,
+        );
+
+    }
+}
