7ae1e66ae72228fb280e0a99ec256968d2d1fa8a
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-11-10 10:14:28 +0500

    BRIS-41: add CommentResource

---
+++ src/app/Resources/Api/V1/CommentResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Resources\Api\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request = []): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'video_id' => $this->video_id,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
