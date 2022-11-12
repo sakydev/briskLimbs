af41f2be60b79654fe931444f2bedd582d2fd6a0
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-11-12 12:49:39 +0500

    UserControllerTests: add `bio` and `channel_name` in `UserResource`

--- src/app/Resources/Api/V1/UserResource.php
+++ src/app/Resources/Api/V1/UserResource.php
@@ -29,6 +29,8 @@ class UserResource extends JsonResource
             'username' => $this->username,
             'email' => $this->email,
             'status' => $this->status,
+            'bio' => $this->bio,
+            'channel_name' => $this->channel_name,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
