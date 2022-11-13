dc75c81b53294df90b9b0ab5341f2aaec7e75366
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-11-13 16:53:38 +0500

    VideoControllerTests: add `convertToKB` and `getMaxUploadSizeInKB` helpers

--- src/app/Helpers/helpers.php
+++ src/app/Helpers/helpers.php
@@ -17,6 +17,23 @@ function convertToBytes(string $size): float {
     return (float) $size;
 }
 
+function convertToKB(string $size): float {
+    $parsedSize = (float) $size;
+    if (str_contains($size, 'M')) {
+        return $parsedSize * 1024;
+    }
+
+    if (str_contains($size, 'G')) {
+        return ($parsedSize * 1024) * 1024;
+    }
+
+    if (str_contains($size, 'T')) {
+        return (($parsedSize * 1024) * 1024) * 1024;
+    }
+
+    return (float) $size;
+}
+
 function convertToMB(mixed $inputSize): float {
     if (str_contains($inputSize, 'M')) {
         return (float) $inputSize;
@@ -31,4 +48,12 @@ function convertToMB(mixed $inputSize): float {
 
 function convertBytesToMB(float $bytes): float {
     return round(($bytes / 1024) / 1024, 2);
+}
+
+function getMaxUploadSizeInKB(): float {
+    $iniMaxFilesize = convertToKB(ini_get('upload_max_filesize'));
+    $configMaxFilesize = config('settings.max_filesize_video'); // already in KB
+
+    // always prefer ini if it is smaller to prevent unexpected errors
+    return $iniMaxFilesize < $configMaxFilesize ? $iniMaxFilesize : $configMaxFilesize;
 }
