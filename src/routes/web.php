42191c16f27f60c334d344ab3c36a9a6cae9e3aa
Saqib Razzaq <saqib.razzaq@plana.earth>
2022-11-18 18:34:47 +0500

    Refactoring: delete web controllers for now

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,6 @@
 <?php declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Auth;
 
 /*
@@ -22,18 +20,18 @@ Route::view('/', 'home');
 Route::view('/blank', 'blank');
 Route::view('/admin/blank', 'admin/blank');
 
-Route::prefix('videos')->middleware('auth')->group(function () {
+/*Route::prefix('videos')->middleware('auth')->group(function () {
     Route::post('/', [VideoController::class, 'store'])->name('store_video');
     Route::put('/{video}', [VideoController::class, 'update'])->name('update_video');
     Route::delete('/{video}', [VideoController::class, 'destroy'])->name('destroy_video');
 
     Route::get('/upload', [VideoController::class, 'create'])->name('upload_video');
-});
+});*/
 
-Route::prefix('videos')->group(function () {
+/*Route::prefix('videos')->group(function () {
     Route::get('/', [VideoController::class, 'index'])->name('list_videos');
     Route::get('/{video}', [VideoController::class, 'show'])->name('show_video');
-});
+});*/
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/pages/{page}', [PageController::class, 'show'])->name('view-page');
+/*Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/pages/{page}', [PageController::class, 'show'])->name('view-page');*/
